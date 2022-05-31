007745c61c61293b6c87765cb5bda51da3fbfb9f
Samaresh-Das <samareshmail679@gmail.com>
2022-05-31 13:25:16 +0530

    Finished

---
+++ app.js
@@ -0,0 +1,65 @@
+// const p1Button = document.querySelector("#p1Button");
+// const p1Display = document.querySelector("#p1Display");
+const p1 = {
+  score: 0,
+  button: document.querySelector("#p1Button"),
+  display: document.querySelector("#p1Display"),
+};
+const p2 = {
+  score: 0,
+  button: document.querySelector("#p2Button"),
+  display: document.querySelector("#p2Display"),
+};
+// const p2Button = document.querySelector("#p2Button");
+// const p2Display = document.querySelector("#p2Display");
+const resetButton = document.querySelector("#reset");
+const playto = document.querySelector("#playto");
+
+// let p1Score = 0;
+// let p2Score = 0;
+let winningScore = 3;
+let isGameOver = false;
+
+function updateScore(player, opponent) {
+  if (!isGameOver) {
+    player.score += 1;
+    if (player.score === winningScore) {
+      isGameOver = true;
+      player.display.classList.add("has-text-success");
+      opponent.display.classList.add("has-text-danger");
+      btndisabled(true);
+    }
+    player.display.textContent = player.score;
+  }
+}
+
+p1.button.addEventListener("click", function () {
+  updateScore(p1, p2);
+});
+
+p2.button.addEventListener("click", function () {
+  updateScore(p2, p1);
+});
+
+playto.addEventListener("change", function () {
+  winningScore = parseInt(this.value);
+  reset();
+});
+
+resetButton.addEventListener("click", reset);
+
+function reset() {
+  isGameOver = false;
+  p1.score = 0;
+  p2.score = 0;
+  p1.display.textContent = 0;
+  p2.display.textContent = 0;
+  p1.display.classList.remove("has-text-success", "has-text-danger");
+  p2.display.classList.remove("has-text-success", "has-text-danger");
+  btndisabled(false);
+}
+
+function btndisabled(value) {
+  p1.button.disabled = value;
+  p2.button.disabled = value;
+}
